05c4be890ce6bb6a3862b91bdd1019bcd23fda19
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-18 01:04:48 +0100

    update.sh: Add double quotes to prevent globbing

--- rootfs/update.sh
+++ rootfs/update.sh
@@ -2,7 +2,7 @@
 
 echo -e "\e[34m Updatig repo!\e[39m"
 aur sync --no-view --noconfirm \
-	-d ${REPO_NAME} \
+	-d "${REPO_NAME}" \
 	-r /repo \
 	-R -u &> /dev/null
 echo -e "\e[32m Repo sucessfully updated!\e[39m"
