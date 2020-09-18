910a03e92f92d18dc46e44d1b6a9e0102bb9f278
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-18 01:02:48 +0100

    update.sh: Add flag to remove old packages

--- rootfs/update.sh
+++ rootfs/update.sh
@@ -4,5 +4,5 @@ echo -e "\e[34m Updatig repo!\e[39m"
 aur sync --no-view --noconfirm \
 	-d ${REPO_NAME} \
 	-r /repo \
-	-u &> /dev/null
+	-R -u &> /dev/null
 echo -e "\e[32m Repo sucessfully updated!\e[39m"
