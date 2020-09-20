dd913b30ee18db8ba88c822597f0c13e0a8cd5d8
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-20 19:10:31 +0100

    remove.sh: Case when $1 isn't passed and prints

--- rootfs/remove.sh
+++ rootfs/remove.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-repo-remove /repo/"${REPO_NAME}".db.tar.xz "$1"       
-rm -rf /repo/"$1"*
+if [ -z "$1"];then
+    echo -e "\e[31mYou forgot to pass the package name!\e[39m"
+else
+    echo -e "\e[33mRemoving${1} from repo!\e[39m"
+    repo-remove /repo/"${REPO_NAME}".db.tar.xz "$1" &> /dev/null     
+    rm -rf /repo/"$1"* &> /dev/null
+    echo -e "\e[32mSucessfully removed ${1} from the repo!\e[39m"
+fi
