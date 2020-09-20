b2e51e12d67233de5e268f31ccdb760348bad4db
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-09-20 17:39:41 +0100

    start.sh: Cron output add instead of replacing

--- rootfs/start.sh
+++ rootfs/start.sh
@@ -37,7 +37,7 @@ setup ()
 	ln -sf /usr/share/zoneinfo/"$TZ" /etc/localtime
 
 	# Add Cronjob
-	echo "${CRON} /bin/bash /update.sh > /home/abc/cron_repo" > /var/spool/cron/abc
+	echo "${CRON} /bin/bash /update.sh >> /home/abc/cron_repo" > /var/spool/cron/abc
 }
 
 send_notification ()
